6de3315280b192e3e84a3ddea83017b502bd552a
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-08-18 19:06:24 -0300

    Add experimental R2 and change CLUT location for tiles

---
+++ assets/levels/R2/tiles16.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles16" tilewidth="16" tileheight="16" tilecount="832" columns="16">
+ <image source="16x16.png" width="256" height="832"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0">
+    <polygon points="0,0 0,16 -11.9462,15.9227 -11.9462,-0.0835836"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
