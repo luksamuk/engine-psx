8a246f662513864890acc242fc03d2cd22ff30ec
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-09-11 01:03:07 -0300

    Add more parallax elements to title screen

--- src/screen_title.c
+++ src/screen_title.c
@@ -18,21 +18,21 @@ typedef struct {
     uint8_t mode;
 } texture_props;
 
-/* Parallax data
-   WATER -- total size: 24
-   P0: 64x6, v0 = 0
-   P1: 64x5, v0 = 6
-   P2: 62x5, v0 = 11
-   P3: 64x5, v0 = 16
-   P4: 66x4, v0 = 20
- */
-#define PRL_INFO_PER_PIECE 4
-static const int16_t prl_water_data[] = {
-    64, 6,  0, 0x0666,
-    64, 5,  6, 0x0800,
-    62, 5, 11, 0x099a,
-    64, 5, 16, 0x0b33,
-    66, 4, 20, 0x0ccd,
+/* Parallax data */
+#define PRL_INFO_PER_PIECE 6
+#define PRL_NUM_PIECES     7
+static const int16_t prl_data[] = {
+    //  W    H  v0  spd      ypos  single
+    // Island, immovable
+    256, 47, 62, 0x0000,  170,  1,
+    // Cloud data
+    256, 37, 25, 0x019a,  181,  0,
+    // Water data
+    64,   6,  0, 0x0666,  216,  0,
+    64,   5,  6, 0x0800,  222,  0,
+    62,   5, 11, 0x099a,  227,  0,
+    64,   5, 16, 0x0b33,  232,  0,
+    66,   4, 20, 0x0ccd,  237,  0,
 };
 
 typedef struct {
@@ -42,7 +42,7 @@ typedef struct {
     uint8_t menu_option;
     uint8_t selected;
     uint8_t next_scene;
-    int32_t water_pos[5];
+    int32_t prl_pos[PRL_NUM_PIECES];
 } screen_title_data;
 
 
@@ -74,26 +74,20 @@ screen_title_load()
     screen_title_data *data = screen_alloc(sizeof(screen_title_data));
 
     title_load_texture("\\SPRITES\\TITLE\\TITLE.TIM;1", &data->props_title);
-    printf("Title -- mode: %d, x: %d, y: %d\n",
-           data->props_title.mode,
-           data->props_title.prect_x,
-           data->props_title.prect_y);
-    
     title_load_texture("\\SPRITES\\TITLE\\PRL.TIM;1", &data->props_prl);
-    printf("Parallax data -- mode: %d, x: %d, y: %d\n",
-           data->props_prl.mode,
-           data->props_prl.prect_x,
-           data->props_prl.prect_y);
 
     data->rgb_count = 0;
     data->menu_option = 0;
     data->selected = 0;
     data->next_scene = 0;
 
-    bzero(data->water_pos, 5 * sizeof(int32_t));
+    bzero(data->prl_pos, PRL_NUM_PIECES * sizeof(int32_t));
+    data->prl_pos[0] = 32 << 12; // Island center
 
     sound_stop_xa();
     sound_play_xa("\\BGM\\BGM003.XA;1", 0, 1, 0);
+
+    set_clear_color(56, 104, 200);
 }
 
 void
@@ -108,11 +102,11 @@ screen_title_update(void *d)
 {
     screen_title_data *data = (screen_title_data *)d;
 
-    for(int wp = 0; wp < 5; wp++) {
-        int32_t wp_spd = prl_water_data[(wp * PRL_INFO_PER_PIECE) + 3];
-        int32_t wp_w = ((int32_t)prl_water_data[(wp * PRL_INFO_PER_PIECE)] << 12);
-        data->water_pos[wp] -= wp_spd;
-        if(data->water_pos[wp] < -wp_w) data->water_pos[wp] = 0;
+    for(int wp = 0; wp < PRL_NUM_PIECES; wp++) {
+        int32_t wp_spd = prl_data[(wp * PRL_INFO_PER_PIECE) + 3];
+        int32_t wp_w = ((int32_t)prl_data[(wp * PRL_INFO_PER_PIECE)] << 12);
+        data->prl_pos[wp] -= wp_spd;
+        if(data->prl_pos[wp] < -wp_w) data->prl_pos[wp] = 0;
     }
     
     if(!data->selected) {
@@ -166,10 +160,10 @@ screen_title_drawtitle(screen_title_data *data)
                            data->props_title.prect_y);
     poly->clut = 0;
     setXY4(poly,
-           32,       24,
-           32 + 256, 24,
-           32,       24 + 175,
-           32 + 256, 24 + 175);
+           32,       20,
+           32 + 256, 20,
+           32,       20 + 175,
+           32 + 256, 20 + 175);
     setUV4(poly,
            0,   0,
            255, 0,
@@ -179,16 +173,17 @@ screen_title_drawtitle(screen_title_data *data)
 }
 
 static void
-screen_title_drawwater(screen_title_data *data)
+screen_title_drawprl(screen_title_data *data)
 {
-    int16_t y = SCREEN_YRES - 24;
-    for(int p = 0; p < 5; p++) {
+    for(int p = 0; p < PRL_NUM_PIECES; p++) {
         int idx = (p * PRL_INFO_PER_PIECE);
-        int16_t w  = prl_water_data[idx];
-        int16_t h  = prl_water_data[idx + 1];
-        int16_t v0 = prl_water_data[idx + 2];
+        int32_t w  = prl_data[idx];
+        int32_t h  = prl_data[idx + 1];
+        int32_t v0 = prl_data[idx + 2];
+        int32_t y  = prl_data[idx + 4];
+        int32_t s  = prl_data[idx + 5];
 
-        for(int32_t wx = data->water_pos[p];
+        for(int32_t wx = data->prl_pos[p];
             wx < ((int32_t)(SCREEN_XRES + w) << 12);
             wx += ((int32_t)w << 12)) {
             int16_t x = (int16_t)(wx >> 12);
@@ -208,13 +203,13 @@ screen_title_drawwater(screen_title_data *data)
                    x + w, y + h);
             setUV4(poly,
                    0, v0,
-                   w, v0,
+                   w - 1, v0,
                    0, v0 + h,
-                   w, v0 + h);
+                   w - 1, v0 + h);
             sort_prim(poly, OT_LENGTH - 1);
-        }
 
-        y += h;
+            if(s) break;
+        }
     }
 }
 
@@ -223,11 +218,11 @@ screen_title_draw(void *d)
 {
     screen_title_data *data = (screen_title_data *)d;
     screen_title_drawtitle(data);
-    screen_title_drawwater(data);
+    screen_title_drawprl(data);
 
     if(data->rgb_count >= 128) {
         const char *text = menu_text[data->menu_option];
-        draw_text(CENTERX - (strlen(text) << 2), 199, 0, text);
+        draw_text(CENTERX - (strlen(text) << 2), 204, 0, text);
 
         char buffer[255] = { 0 };
         snprintf(buffer, 255, "%s %s", __DATE__, __TIME__);
