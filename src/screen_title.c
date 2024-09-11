b78bc4b0e0c693ca6a13766c242a452b62cc6c7b
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-09-10 23:59:24 -0300

    Add water parallax to title screen

--- src/screen_title.c
+++ src/screen_title.c
@@ -1,6 +1,6 @@
 #include "screens/title.h"
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
 #include <stdio.h>
 
 #include "util.h"
@@ -18,12 +18,31 @@ typedef struct {
     uint8_t mode;
 } texture_props;
 
+/* Parallax data
+   WATER -- total size: 24
+   P0: 64x6, v0 = 0
+   P1: 64x5, v0 = 6
+   P2: 62x5, v0 = 11
+   P3: 64x5, v0 = 16
+   P4: 66x4, v0 = 20
+ */
+#define PRL_INFO_PER_PIECE 4
+static const int16_t prl_water_data[] = {
+    64, 6,  0, 0x0666,
+    64, 5,  6, 0x0800,
+    62, 5, 11, 0x099a,
+    64, 5, 16, 0x0b33,
+    66, 4, 20, 0x0ccd,
+};
+
 typedef struct {
     texture_props props_title;
+    texture_props props_prl;
     uint8_t rgb_count;
     uint8_t menu_option;
     uint8_t selected;
     uint8_t next_scene;
+    int32_t water_pos[5];
 } screen_title_data;
 
 
@@ -40,12 +59,12 @@ static void
 title_load_texture(const char *filename, texture_props *props)
 {
     uint32_t length;
-    TIM_IMAGE titlecard;
+    TIM_IMAGE img;
     uint8_t *data = file_read(filename, &length);
-    load_texture(data, &titlecard);
-    props->mode = titlecard.mode;
-    props->prect_x  = titlecard.prect->x;
-    props->prect_y  = titlecard.prect->y;
+    load_texture(data, &img);
+    props->mode = img.mode;
+    props->prect_x  = img.prect->x;
+    props->prect_y  = img.prect->y;
     free(data);
 }
 
@@ -53,12 +72,25 @@ void
 screen_title_load()
 {
     screen_title_data *data = screen_alloc(sizeof(screen_title_data));
+
     title_load_texture("\\SPRITES\\TITLE\\TITLE.TIM;1", &data->props_title);
+    printf("Title -- mode: %d, x: %d, y: %d\n",
+           data->props_title.mode,
+           data->props_title.prect_x,
+           data->props_title.prect_y);
+    
+    title_load_texture("\\SPRITES\\TITLE\\PRL.TIM;1", &data->props_prl);
+    printf("Parallax data -- mode: %d, x: %d, y: %d\n",
+           data->props_prl.mode,
+           data->props_prl.prect_x,
+           data->props_prl.prect_y);
 
     data->rgb_count = 0;
     data->menu_option = 0;
     data->selected = 0;
     data->next_scene = 0;
+
+    bzero(data->water_pos, 5 * sizeof(int32_t));
 
     sound_stop_xa();
     sound_play_xa("\\BGM\\BGM003.XA;1", 0, 1, 0);
@@ -75,6 +107,14 @@ void
 screen_title_update(void *d)
 {
     screen_title_data *data = (screen_title_data *)d;
+
+    for(int wp = 0; wp < 5; wp++) {
+        int32_t wp_spd = prl_water_data[(wp * PRL_INFO_PER_PIECE) + 3];
+        int32_t wp_w = ((int32_t)prl_water_data[(wp * PRL_INFO_PER_PIECE)] << 12);
+        data->water_pos[wp] -= wp_spd;
+        if(data->water_pos[wp] < -wp_w) data->water_pos[wp] = 0;
+    }
+    
     if(!data->selected) {
         if(data->rgb_count < 128)
             data->rgb_count += 4;
@@ -113,28 +153,77 @@ screen_title_update(void *d)
     }
 }
 
+static void
+screen_title_drawtitle(screen_title_data *data)
+{
+    POLY_FT4 *poly = (POLY_FT4 *)get_next_prim();
+    increment_prim(sizeof(POLY_FT4));
+    setPolyFT4(poly);
+    setRGB0(poly, data->rgb_count, data->rgb_count, data->rgb_count);
+    poly->tpage = getTPage(data->props_title.mode & 0x3,
+                           0,
+                           data->props_title.prect_x,
+                           data->props_title.prect_y);
+    poly->clut = 0;
+    setXY4(poly,
+           32,       24,
+           32 + 256, 24,
+           32,       24 + 175,
+           32 + 256, 24 + 175);
+    setUV4(poly,
+           0,   0,
+           255, 0,
+           0,   174,
+           255, 174);
+    sort_prim(poly, OT_LENGTH - 1);
+}
+
+static void
+screen_title_drawwater(screen_title_data *data)
+{
+    int16_t y = SCREEN_YRES - 24;
+    for(int p = 0; p < 5; p++) {
+        int idx = (p * PRL_INFO_PER_PIECE);
+        int16_t w  = prl_water_data[idx];
+        int16_t h  = prl_water_data[idx + 1];
+        int16_t v0 = prl_water_data[idx + 2];
+
+        for(int32_t wx = data->water_pos[p];
+            wx < ((int32_t)(SCREEN_XRES + w) << 12);
+            wx += ((int32_t)w << 12)) {
+            int16_t x = (int16_t)(wx >> 12);
+            POLY_FT4 *poly = (POLY_FT4 *)get_next_prim();
+            increment_prim(sizeof(POLY_FT4));
+            setPolyFT4(poly);
+            setRGB0(poly, 128, 128, 128);
+            poly->tpage = getTPage(data->props_prl.mode & 0x3,
+                                   0,
+                                   data->props_prl.prect_x,
+                                   data->props_prl.prect_y);
+            poly->clut = 0;
+            setXY4(poly,
+                   x,     y,
+                   x + w, y,
+                   x,     y + h,
+                   x + w, y + h);
+            setUV4(poly,
+                   0, v0,
+                   w, v0,
+                   0, v0 + h,
+                   w, v0 + h);
+            sort_prim(poly, OT_LENGTH - 1);
+        }
+
+        y += h;
+    }
+}
+
 void
 screen_title_draw(void *d)
 {
     screen_title_data *data = (screen_title_data *)d;
-
-    SPRT *sprite = (SPRT *)get_next_prim();
-    increment_prim(sizeof(SPRT));
-    setSprt(sprite);
-    setXY0(sprite, 32, 24);
-    setWH(sprite, 256, 175);
-    setUV0(sprite, 0, 0);
-    setRGB0(sprite, data->rgb_count, data->rgb_count, data->rgb_count);
-    sort_prim(sprite, OT_LENGTH - 1);
-
-    DR_TPAGE *tpage = (DR_TPAGE *)get_next_prim();
-    increment_prim(sizeof(DR_TPAGE));
-    setDrawTPage(tpage, 0, 1,
-                 getTPage(data->props_title.mode & 0x3,
-                          0,
-                          data->props_title.prect_x,
-                          data->props_title.prect_y));
-    sort_prim(tpage, OT_LENGTH - 1);
+    screen_title_drawtitle(data);
+    screen_title_drawwater(data);
 
     if(data->rgb_count >= 128) {
         const char *text = menu_text[data->menu_option];
