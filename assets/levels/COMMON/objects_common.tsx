fa8285268a3e6eaf17bac1645cb16333e52bb9ca
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-09-26 22:39:31 -0300

    Evolution with object data and placement design

--- assets/levels/COMMON/objects_common.tsx
+++ assets/levels/COMMON/objects_common.tsx
@@ -6,15 +6,46 @@
   <properties>
    <property name="Kind" value="NONE"/>
   </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="34" width="30" height="30"/>
+  </objectgroup>
  </tile>
- <tile id="2" type="spikes"/>
+ <tile id="2" type="spikes">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="32" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="3" type="checkpoint"/>
- <tile id="4" type="spring_yellow"/>
- <tile id="5" type="spring_red"/>
- <tile id="6" type="spring_yellow_diagonal"/>
- <tile id="7" type="spring_red_diagonal"/>
+ <tile id="4" type="spring_yellow">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="48" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="spring_red">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="48" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6" type="spring_yellow_diagonal">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="17" y="32">
+    <polygon points="0,0 9.69078,0 31.641,22.1837 31.641,32.2248 -0.583782,32.108"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7" type="spring_red_diagonal">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="17" y="32">
+    <polygon points="0,0 9.69078,0 31.641,22.1837 31.641,32.2248 -0.583782,32.108"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="8" type="ring_3h"/>
- <tile id="9" type="switch"/>
+ <tile id="9" type="switch">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="48" width="32" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="10" type="ring_3v"/>
  <tile id="11" type="goal_sign"/>
 </tileset>
