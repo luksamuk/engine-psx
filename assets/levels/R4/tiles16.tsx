1ca8b3d29ecc48a30b6672232a2e7d9b2d41cc4a
Lucas Vieira <lucasvieira@protonmail.com>
2025-05-30 22:46:45 -0300

    Add tile data for R4

---
+++ assets/levels/R4/tiles16.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles16" tilewidth="16" tileheight="16" tilecount="624" columns="16">
+ <image source="16x16.png" width="256" height="624"/>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+</tileset>
