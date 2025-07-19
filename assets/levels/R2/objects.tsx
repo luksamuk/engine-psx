15a6cfe6a630b40726cc5356674449d8ee3905c6
Lucas Vieira <lucasvieira@protonmail.com>
2025-07-19 02:23:31 -0300

    [GHZ] Add Buzzbomber animations

--- assets/levels/R2/objects.tsx
+++ assets/levels/R2/objects.tsx
@@ -2,4 +2,5 @@
 <tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="64" tileheight="64" tilecount="4" columns="4">
  <image source="obj_designtiles.png" width="256" height="64"/>
  <tile id="0" type="motobug"/>
+ <tile id="1" type="buzzbomber"/>
 </tileset>
